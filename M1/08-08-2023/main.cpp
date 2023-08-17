42dc2c7ca289e70e091e0a469c1fb6b1d96699c8
Rafael Mota Alves <aniratak2415@gmail.com>
2023-08-17 12:44:53 -0300

    feat: Adjusting the code pattern and adding an insert function in descending order

--- M1/08-08-2023/main.cpp
+++ M1/08-08-2023/main.cpp
@@ -9,21 +9,21 @@ struct Node
 };
 
 template <typename T>
-struct LSE
+struct LUE
 {
     Node<T> *start;
     Node<T> *end;
 };
 
 template <typename T>
-void inicializateLSE(LSE<T> &list)
+void inicializeLUE(LUE<T> &list)
 {
     list.start = NULL;
     list.end = NULL;
 }
 
 template <typename T>
-void showLSE(LSE<T> list)
+void showLUE(LUE<T> list)
 {
     Node<T> *aux = list.start;
 
@@ -37,17 +37,17 @@ void showLSE(LSE<T> list)
 }
 
 template <typename T>
-bool insertLSE(LSE<T> &list, T valor)
+bool insertLUE(LUE<T> &list, T value)
 {
+
     Node<T> *novo;
 
     novo = new Node<T>;
     if (novo == NULL)
         return false;
-    novo->info = valor;
+    novo->info = value;
     novo->elo = NULL;
 
-    // empty list
     if (list.start == NULL)
     {
         list.start = novo;
@@ -55,25 +55,22 @@ bool insertLSE(LSE<T> &list, T valor)
         return true;
     }
 
-    // Put the value at the beginning of the list
-    if (valor < list.start->info)
+    if (value <= list.start->info)
     {
         novo->elo = list.start;
         list.start = novo;
         return true;
     }
 
-    // put the value at the end of the list
-    if (valor > list.end->info)
+    if (value >= list.end->info)
     {
         list.end->elo = novo;
         list.end = novo;
         return true;
     }
 
-    // put the value in the middle of the list
     Node<T> *aux = list.start;
-    while (aux->info < valor && aux->elo->info < valor)
+    while (aux->info < value && aux->elo->info <= value)
         aux = aux->elo;
 
     novo->elo = aux->elo;
@@ -81,22 +78,59 @@ bool insertLSE(LSE<T> &list, T valor)
     return true;
 }
 
-//
-// Always insert the given value End of list node
-//
 template <typename T>
-bool inserirFinalLSE(LSE<T> &list, T valor)
+bool insertLUE_D(LUE<T> &list, T value)
 {
+
     Node<T> *novo;
 
-    // Create a new node
     novo = new Node<T>;
     if (novo == NULL)
         return false;
-    novo->info = valor;
+    novo->info = value;
     novo->elo = NULL;
 
-    // empty list
+    if (list.start == NULL)
+    {
+        list.start = novo;
+        list.end = novo;
+        return true;
+    }
+
+    if (value >= list.start->info)
+    {
+        novo->elo = list.start;
+        list.start = novo;
+        return true;
+    }
+
+    if (value <= list.end->info)
+    {
+        list.end->elo = novo;
+        list.end = novo;
+        return true;
+    }
+
+    Node<T> *aux = list.start;
+    while (aux->info > value && aux->elo->info >= value)
+        aux = aux->elo;
+
+    novo->elo = aux->elo;
+    aux->elo = novo;
+    return true;
+}
+
+template <typename T>
+bool insertEndLUE(LUE<T> &list, T value)
+{
+    Node<T> *novo;
+
+    novo = new Node<T>;
+    if (novo == NULL)
+        return false;
+    novo->info = value;
+    novo->elo = NULL;
+
     if (list.start == NULL)
     {
         list.start = novo;
@@ -110,13 +144,13 @@ bool inserirFinalLSE(LSE<T> &list, T valor)
 }
 
 template <typename T>
-Node<T> *searchLSE(LSE<T> list, T valor)
+Node<T> *searchLUE(LUE<T> list, T value)
 {
     Node<T> *aux = list.start;
 
     while (aux != NULL)
     {
-        if (aux->info == valor)
+        if (aux->info == value)
             return aux;
         aux = aux->elo;
     }
@@ -124,36 +158,36 @@ Node<T> *searchLSE(LSE<T> list, T valor)
 }
 
 template <typename T>
-bool removeLSE(LSE<T> &list, T valor)
+bool removeLUE(LUE<T> &list, T value)
 {
-    Node<T> *aux = list.start, *anterior = NULL;
+    Node<T> *aux = list.start, *previous = NULL;
 
-    while (aux != NULL && aux->info != valor)
+    while (aux != NULL && aux->info != value)
     {
-        anterior = aux;
+        previous = aux;
         aux = aux->elo;
     }
     if (aux == NULL)
         return false;
 
     if (aux == list.start)
-    { // Cases 1 e 2
+    {
         list.start = list.start->elo;
         if (aux == list.end)
             list.end = NULL;
     }
     else
-    { // Cases 3 e 4
-        anterior->elo = aux->elo;
+    {
+        previous->elo = aux->elo;
         if (aux == list.end)
-            list.end = anterior;
+            list.end = previous;
     }
     delete aux;
     return true;
 }
 
 template <typename T>
-void releaseLSE(LSE<T> &list)
+void liberarLUE(LUE<T> &list)
 {
     Node<T> *aux = list.start;
     Node<T> *aux2;
@@ -164,57 +198,4 @@ void releaseLSE(LSE<T> &list)
         delete aux;
         aux = aux2;
     }
-}
-
-int main()
-{
-    LSE<char> list1;
-
-    inicializateLSE(list1);
-
-    insertLSE(list1, 'P');
-    insertLSE(list1, 'E');
-    insertLSE(list1, 'R');
-    insertLSE(list1, 'N');
-    insertLSE(list1, 'A');
-    insertLSE(list1, 'M');
-    insertLSE(list1, 'B');
-    insertLSE(list1, 'U');
-    insertLSE(list1, 'C');
-    insertLSE(list1, 'O');
-
-    cout << endl
-         << "list 1: ";
-    showLSE(list1);
-
-    cout << endl;
-    Node<char> *aux = searchLSE(list1, 'M');
-    if (aux == NULL)
-        cout << "Informacao nao localizada";
-    else
-        cout << "Informacao esta na list";
-
-    removeLSE(list1, 'A');
-    removeLSE(list1, 'U');
-    removeLSE(list1, 'N');
-    removeLSE(list1, 'E');
-    removeLSE(list1, 'R');
-    removeLSE(list1, 'O');
-
-    cout << endl
-         << "list 1: ";
-    showLSE(list1);
-
-    removeLSE(list1, 'B');
-    removeLSE(list1, 'C');
-    removeLSE(list1, 'M');
-    removeLSE(list1, 'P');
-
-    cout << endl
-         << "list 1: ";
-    showLSE(list1);
-
-    releaseLSE(list1);
-
-    cout << endl;
 }
